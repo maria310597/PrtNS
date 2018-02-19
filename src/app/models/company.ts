27b6c473791b6cbffacde4d7bedf11b91016d91f
maria310597 <36638103+maria310597@users.noreply.github.com>
2018-02-19 18:44:12 +0100

    Report&Company

--- src/app/models/company.ts
+++ src/app/models/company.ts
@@ -1,2 +1,16 @@
 export class Company {
+    name: String;
+    email: String;
+    billMail: String;
+    faxNumber: String;
+    tlf: String;
+
+    constructor(name: String, email: String, billMail: String, faxNumber: String, tlf: String){
+        this.name = name;
+        this.email = email;
+        this.billMail = billMail;
+        this.faxNumber = faxNumber;
+        this.tlf = tlf;
+
+    }
 }
--- src/app/models/report.ts
+++ src/app/models/report.ts
@@ -1,2 +1,34 @@
+import { User } from "./user";
+import { Company } from "./company";
+
 export class Report {
+    id: number;
+    operator: User;
+    date: Date;
+    company: Company;
+    dBegining: Date;
+    dEnd: Date;
+    notes: String[5];
+    km: number;
+    displacements: number;
+    parking: number;
+    
+    
+
+
+    constructor(id: number, opertor: User, date: Date, company: Company, dBegining: Date, dEnd: Date,
+         notes:String[5], km: number,displacements: number, parking: number ){
+        this.id = id;
+        this.operator = this.operator;
+        this.date = date;
+        this.company = company;
+        this.dBegining = dBegining;
+        this.dEnd = dEnd;
+        this.notes = notes;
+        this.km = km;
+        this.displacements = displacements;
+        this.parking = parking;
+       
+
+    }
 }
