eeb7b9fbb615feac14c6aeacae58efabcc6feaf0
maria310597 <36638103+maria310597@users.noreply.github.com>
2018-02-28 18:30:14 +0100

    update and remove

--- src/app/models/company.ts
+++ src/app/models/company.ts
@@ -1,16 +1,20 @@
 export class Company {
-    name: String;
-    email: String;
-    billMail: String;
-    faxNumber: String;
-    tlf: String;
+    name: string; //Clave primaria
+    email: string;
+    billMail: string;
+    faxNumber: string;
+    tlf: string;
+    lastmovement: Date;
+    igualada: boolean;
 
-    constructor(name: String, email: String, billMail: String, faxNumber: String, tlf: String) {
+    constructor(name: string, email: string, lastmovement: Date, igualada: boolean, billMail: string, faxNumber: string, tlf: string) {
         this.name = name;
         this.email = email;
         this.billMail = billMail;
         this.faxNumber = faxNumber;
         this.tlf = tlf;
+        this.igualada = igualada;
+        this.lastmovement = lastmovement;
 
     }
 }
--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Company } from '../models/company';
 
 
@@ -10,10 +10,43 @@ import { Company } from '../models/company';
 export class CompanyService {
 
   readonly path = '/company';
-  constructor(private afs: AngularFirestore) { }
+  companies$: Observable<Company[]>;
+  companyCollectionRef: AngularFirestoreCollection<Company>;
 
+  constructor(private afs: AngularFirestore) {
+    this.companyCollectionRef = this.afs.collection<Company>(this.path);
+    this.companies$ = this.companyCollectionRef.snapshotChanges().map(actions => {
+      return actions.map(action => {
+        const data = action.payload.doc.data() as Company;
+        const id = action.payload.doc.id;
+        return { id, ...data };
+      });
+    });
+  }
   getCollection$(): Observable<Company[]> {
     return this.afs.collection<Company>(this.path).valueChanges();
   }
 
+  add(Company: Company) {
+    if (Company ) {
+      this.companyCollectionRef.add({ name: Company.name, email:Company.email, 
+                                      billMail:Company.billMail,
+                                      faxNumber:Company.faxNumber,
+                                      igualada: Company.igualada,
+                                      lastmovement:Company.lastmovement,tlf:Company.tlf});
+    }
+  }
+  updateTodo(Company: Company) {
+    this.companyCollectionRef.doc(Company.name).update({ name: !Company.name,
+                                                          email: !Company.email, 
+                                                          billMail: !Company.billMail,
+                                                          faxNumber: !Company.faxNumber,
+                                                          igualada: !Company.igualada,
+                                                          lastmovement: !Company.lastmovement,
+                                                          tlf: !Company.tlf });
+  }
+  
+  deleteTodo(Company: Company) {
+    this.companyCollectionRef.doc(Company.name).delete();
+  }
 }
